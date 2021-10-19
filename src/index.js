8f8700b0217ef93c5a1d310c5bacabd9255b8931
yota-g <64677279+yota-g@users.noreply.github.com>
2021-10-20 00:28:31 +0900

    learn 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import "./styles.css";
+/**
+ * const let等の変数宣言
+ **/
+// var val1 = "var変数";
+// console.log(val1);
+// 変数の上書き、再宣言ができてしまう
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+//letは上書きが可能
+// val2 = "let変数を上書き"
+// console.log(val2);
+//再宣言は不可能
+// let val2 ="変数を再宣言"
+//設定した時点でエラーが出る。
+
+//上書きも再宣言も不可能
+// const val3 = "const変数を上書き"
+// console.log(val3);
+
+// val3 = "上書き"
+//constはread-onlyが表示されている。
+//constに設定したオブジェクトや配列は値を変えることが可能
+//新しいプロパティの追加も可能。
+// const  val4 = {
+//   name: "tanaka",
+//   age: 23
+// }
+// console.log(val4);
+// val4.address = "Hiroshima";
+// val4.name = "kinoshita"
+// console.log(val4);
+//プロパティの追加、変更可能。
+//基本的にはconstしか使用しない。
+
+// const val5 = ['dog', 'cat'];
+// console.log(val5);
+// val5[0] = 'zoo';
+// val5.push("monkey")
+// console.log(val5);
+
+// template文字列 文字列の中に変数を組み込みめるようになったES2015
