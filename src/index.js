cc48ffdf48111fb5f82971ba7f3dab8389905681
yota-g <64677279+yota-g@users.noreply.github.com>
2021-10-20 00:35:15 +0900

    learn 19

--- src/index.js
+++ src/index.js
@@ -40,3 +40,10 @@
 // console.log(val5);
 
 // template文字列 文字列の中に変数を組み込みめるようになったES2015
+const name = "じゃけぇ";
+const age = 28;
+//「私の名前はじゃけぇです。年齢は28歳です。」
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1 + "2");
+console.log(`私の名前は${name}です。年齢は${age}です。`);
