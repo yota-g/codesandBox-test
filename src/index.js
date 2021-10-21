207eb194f554e0942d073f545ecac7ce9c230328
yota-g <64677279+yota-g@users.noreply.github.com>
2021-10-21 23:02:04 +0900

    learn 32

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化することが可能に
+  // alert("buttonclick");
+  const inputText = document.getElementById("add-text").value;
+  //valueでinputの情報を取得することが可能。
+  // console.log(inputText);
+  document.getElementById("add-text").value = ""; //追加後にinput内を空に設定
+  //liとdivを作成する。
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // console.log(div, li)
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  // console.log(p);
+  //liの下にdiv、divの下にpを入れていく。
+  let liContent = li.appendChild(div);
+  liContent.appendChild(p);
+  liContent.appendChild();
+  // console.log(li);
+  //divタグの子要素に各要素を設定。
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
